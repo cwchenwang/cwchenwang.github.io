67e7d232cb37910d31cc05170db9f61d209e24cf
Chen Wang <chwangthu@gmail.com>
2022-09-27 15:57:39 +0800

    update cv and add header

--- script.js
+++
@@ -1,8 +0,0 @@
-function expandMisc() {
-    var element = document.getElementsByClassName('collapse-content')[0];
-    if (element.style.display == 'block') {
-        element.style.display = 'none';
-    } else {
-        element.style.display = 'block';
-    }
-}
